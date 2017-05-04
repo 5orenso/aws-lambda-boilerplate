0ed1af71a730a8a7e0b624f25b9e41f17e540b2f
Oistein Sorensen <sorenso@gmail.com>
2017-05-04 16:15:16 +0200

    Added install script.

---
+++ install.sh
@@ -0,0 +1,41 @@
+
+#!/bin/bash
+
+echo 'You should probably do this first:'
+echo '1. Generate a new repo on github.com.'
+echo '2. Clone repo to you project home dir.'
+
+echo '3. Enter path of new repo without trailing slash (ie: "../my-new-web-repo"): '
+read PATH
+
+# Copying paths
+/bin/cp -Rv bin $PATH/.
+/bin/cp -Rv cloudformation $PATH/.
+/bin/cp -Rv test $PATH/.
+
+# Copying files
+/bin/cp -v .coveralls-dist.yml $PATH/.
+/bin/cp -v .gitignore $PATH/.
+/bin/cp -v .jscsrc $PATH/.
+/bin/cp -v .jshintrc $PATH/.
+/bin/cp -v .travis.yml $PATH/.
+/bin/cp -v Gruntfile.js $PATH/.
+/bin/cp -v index.js $PATH/.
+/bin/cp -v install.sh $PATH/.
+/bin/cp -v LICENSE $PATH/.
+/bin/cp -v package.json $PATH/.
+/bin/cp -v README.md $PATH/.
+/bin/cp -v changelog.sh $PATH/.
+/bin/cp -v report-code-coverage.sh $PATH/.
+
+cd $PATH
+/bin/cp -v ./.coveralls-dist.yml ./.coveralls.yml
+
+echo ''
+echo 'Todo:'
+echo '1. Update coverlalls.yml with the correct token.'
+echo '2. Run `npm install`'
+echo '3. Run your tests `grunt`.'
+echo ''
+echo 'All set :)'
+echo ''
